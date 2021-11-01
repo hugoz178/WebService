a724d1142e0bdce7fd50ff6106973f3d74c99bb4
hugoz178 <18040046@alumno.utc.edu.mx>
2021-11-01 16:43:33 -0600

    Cod Conexion
    Escribí el código de conexión

--- conexion.php
+++ conexion.php
@@ -1,3 +1,23 @@
 <?php
 
+class conexion extends PDO
+{
+	private $hostbd = 'localhost';
+	private $nombrebd = 'WebService';
+	private $usuariobd = 'root';
+	private $passwordbd = ' ';
+
+	public dunction __construct()
+	{
+		try{
+			parent::__construct('mysql_host=' . $this->hostbd. ';dbname='. $this->nombrebd . ';charset=utf8', $this->usuariobd, $this->passwordbd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		} catch(PDOException $e){
+			echo 'Error: ' e->getMessage();
+			exit;
+		}
+	}
+
+}	
+	 
+
 ?>
