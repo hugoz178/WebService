fb643871473a42836c30c353eeb165a96d61d7e0
hugoz178 <18040046@alumno.utc.edu.mx>
2021-11-01 18:28:28 -0600

    service.php
    ya busca la id de un registro

--- service.php
+++ service.php
@@ -5,13 +5,23 @@
 	
 	//Listar registros y consultar registro
 	if($_SERVER['REQUEST_METHOD'] == 'GET'){
-
+		if (isset($_GET['id'])) {
+			$sql = $pdo->prepare("SELECT * FROM t_web WHERE id=:id");
+			$sql->bindValue(':id', $_GET['id']);
+			$sql->execute();
+			$sql->setFetchMode(PDO::FETCH_ASSOC);
+			header("HTTP/1.1 200 hay datos");
+			echo json_encode($sql->fetchAll());
+			exit;		
+		} else{
 			$sql = $pdo->prepare("SELECT * FROM t_web");
 			$sql->execute();
 			$sql->setFetchMode(PDO::FETCH_ASSOC);
 			header("HTTP/1.1 200 hay datos");
 			echo json_encode($sql->fetchAll());
-			exit;				
-			
+			exit;	
+
+		}
+		
 	}
 ?>
