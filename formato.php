a176d7f70789ed84c4850a2298d6d349f5795b99
hugoz178 <18040046@alumno.utc.edu.mx>
2021-11-01 18:56:37 -0600

    uptade formato.php
    muestra de diferente forma

--- formato.php
+++ formato.php
@@ -1,4 +1,13 @@
 <?php
-	
+$data = json_decode(file_get_contents("http://localhost/WebService/service.php"), true);
+print_r($data);	
+echo "<br>";
+echo "<br>";
+for ($i=0; $i<count($data); $i++){
+	echo $data[$i]["nombre"]."<br>";
+	echo $data[$i]["p_apellido"]."<br>";
+	echo $data[$i]["s_apellido"]."<br>";
+	echo "<br>";
+}
 
 ?>
