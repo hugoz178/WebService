fa653a521a84a4704b0fa7fd8e11dd2f257a1a79
hugoz178 <18040046@alumno.utc.edu.mx>
2021-11-07 17:45:43 -0600

    modificaciones
    modifique algunas cosas

--- index.php
+++ index.php
@@ -30,23 +30,6 @@
 		include_once 'formato.php';
 
 		 ?>
-	<br><br><br>
-		<div id="map" style="height: 300px; width: 30%;">
-		
-	</div>
-	<script type="text/javascript">
-		function iniciarMap(){
-			var coord = {lat:25.463841202169533 ,lng: -100.9618960436142}
-			var map = new google.maps.Map(document.getElementById('map'),{
-				zoom: 15,
-				center: coord
-			});
-			var marker = new google.maps.Marker({
-      		position: coord,
-      		map: map
-    		});
-		}
-	</script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap"></script>	 
+ 
 </body>
 </html>
